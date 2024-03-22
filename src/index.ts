2363ffb318bfca4e6934a15d8b6b6f8b83e85863
Connor Voisey <connor.voisey@gmail.com>
2024-03-22 21:36:27 +0000

    feat: re-exported typebox

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export { getSchemaBuilder } from './schemas';
 export { getTagBuilder } from './tags';
 export { getPathBuilder } from './paths';
 export type * from './types';
+export { Type } from '@sinclair/typebox';
